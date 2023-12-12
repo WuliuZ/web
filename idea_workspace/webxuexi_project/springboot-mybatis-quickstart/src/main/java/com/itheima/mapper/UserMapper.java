8a376d19fe6f1d3240b9d85dee17bef3fe5fffd7
WuliuZ <1623105262@qq.com>
2023-12-12 21:03:34 +0800

    xml映射文件实现数据库操作

--- idea_workspace/webxuexi_project/springboot-mybatis-quickstart/src/main/java/com/itheima/mapper/UserMapper.java
+++ idea_workspace/webxuexi_project/springboot-mybatis-quickstart/src/main/java/com/itheima/mapper/UserMapper.java
@@ -9,6 +9,8 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
     //查询所有用户信息
-    @Select("select * from user")
+//    @Select("select * from user")
+//    public List<User> list();
+
     public List<User> list();
 }
