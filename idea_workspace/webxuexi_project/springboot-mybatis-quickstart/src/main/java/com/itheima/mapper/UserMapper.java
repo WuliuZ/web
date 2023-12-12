e445401edceb2407268353c7c5d161146bbcdce4
WuliuZ <1623105262@qq.com>
2023-12-12 16:23:38 +0800

    mybatis入门、lombok等
    lombok通过注解简化类中的方法

---
+++ idea_workspace/webxuexi_project/springboot-mybatis-quickstart/src/main/java/com/itheima/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.itheima.mapper;
+
+import com.itheima.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    //查询所有用户信息
+    @Select("select * from user")
+    public List<User> list();
+}
--- idea_workspace/webxuexi_project/springboot-mybatis-quickstart/src/test/java/com/itheima/SpringbootMybatisQuickstartApplicationTests.java
+++ idea_workspace/webxuexi_project/springboot-mybatis-quickstart/src/test/java/com/itheima/SpringbootMybatisQuickstartApplicationTests.java
@@ -1,13 +1,25 @@
 package com.itheima;
 
+import com.itheima.mapper.UserMapper;
+import com.itheima.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.awt.*;
+import java.util.List;
 
 @SpringBootTest
 class SpringbootMybatisQuickstartApplicationTests {
-
+    @Autowired
+    private UserMapper userMapper;
     @Test
-    void contextLoads() {
+    public void testListUser(){
+       List<User> userList=userMapper.list();
+        userList.stream().forEach(user -> {
+            System.out.println(user);
+        });
     }
+
 
 }
